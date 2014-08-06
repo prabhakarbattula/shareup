a75f82ba257a353d24acd0877d3a007ae9eac3c0
battula <prabhakar.battula@gmail.com>
2014-08-06 19:37:50 +0530

    configured for twitter authorisation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,8 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.omniauth :twitter,  AppConfig.twitter['clientId'],
+                             AppConfig.twitter['clientSecret']
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
